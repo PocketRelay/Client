4237f158f5469135271320d653d9cca3cabc6a68
Jacobtread <jacobtread@gmail.com>
2023-09-02 11:56:32 +1200

    Working local http proxy

--- src/servers/http.rs
+++ src/servers/http.rs
@@ -1,8 +1,10 @@
+use crate::TARGET;
 use crate::{constants::HTTP_PORT, show_error};
 use hyper::body::Body;
 use hyper::service::service_fn;
-use hyper::Response;
 use hyper::{server::conn::Http, Request};
+use hyper::{Response, StatusCode};
+use reqwest::Client;
 use std::convert::Infallible;
 use std::{net::Ipv4Addr, process::exit};
 use tokio::net::TcpListener;
@@ -27,17 +29,70 @@ pub async fn start_server() {
 
         tokio::task::spawn(async move {
             if let Err(err) = Http::new()
-                .serve_connection(stream, service_fn(hello))
+                .serve_connection(stream, service_fn(proxy_http))
                 .await
             {
-                println!("Error serving connection: {:?}", err);
+                eprintln!("Failed to serve http connection: {:?}", err);
             }
         });
     }
 }
 
-// An async function that consumes a request, does nothing with it and returns a
-// response.
-async fn hello(req: Request<hyper::body::Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(req.into_body()))
+async fn proxy_http(req: Request<hyper::body::Body>) -> Result<Response<Body>, Infallible> {
+    let path = req
+        .uri()
+        .path_and_query()
+        .map(|value| value.as_str())
+        .unwrap_or_default();
+
+    let target_url = {
+        let target_guard = TARGET.read().await;
+        let target = match target_guard.as_ref() {
+            Some(value) => value,
+            None => {
+                let mut error_response = Response::new(hyper::Body::empty());
+                *error_response.status_mut() = StatusCode::SERVICE_UNAVAILABLE;
+                return Ok(error_response);
+            }
+        };
+
+        format!(
+            "{}://{}:{}{}",
+            target.scheme, target.host, target.port, path
+        )
+    };
+
+    let client = Client::new();
+    let proxy_response = match client
+        .get(target_url)
+        .headers(req.headers().clone())
+        .send()
+        .await
+    {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("Failed to send HTTP request: {}", err);
+            let mut error_response = Response::new(hyper::Body::empty());
+            *error_response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+            return Ok(error_response);
+        }
+    };
+    let status = proxy_response.status();
+    let headers = proxy_response.headers().clone();
+
+    let body = match proxy_response.bytes().await {
+        Ok(value) => value,
+        Err(err) => {
+            eprintln!("Failed to read HTTP response body: {}", err);
+            let mut error_response = Response::new(hyper::Body::empty());
+            *error_response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+            return Ok(error_response);
+        }
+    };
+
+    let mut response = Response::new(hyper::body::Body::from(body));
+    *response.status_mut() = status;
+    *response.headers_mut() = headers;
+
+    Ok(response)
 }
